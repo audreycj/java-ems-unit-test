11b0678f9d2869607abafaff6e085fba1cd95132
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-17 17:09:41 +0800

    Create EmployeeTest.java

---
+++ src/test/java/com/audreynanual/EmployeeTest.java
@@ -0,0 +1,15 @@
+package com.audreynanual;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+public class EmployeeTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
