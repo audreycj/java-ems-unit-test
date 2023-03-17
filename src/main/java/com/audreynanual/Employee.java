a0c4e8a3e60950c2fcfb8317932d41038031e8a3
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-17 17:56:09 +0800

    Added setter methods to Employee class

--- src/main/java/com/audreynanual/Employee.java
+++ src/main/java/com/audreynanual/Employee.java
@@ -7,57 +7,89 @@ public class Employee {
     // instance variables
     private int id;
     private String firstName;
-    private String secondName;
+    private String lastName;
     private int age;
     private String occupation;
     private String department;
     private double salary;
 
     // default constructor method
-    public Employee() {
+    public Employee(int id, String firstName, String lastName) {
 
     }
+
+    // parameterized constructor method
+
 
     // get assigned ID
     public int getID() {
-
+        return id;
     }
 
     // get first name
-
+    public String getFirstName() {
+        return firstName;
+    }
 
     // set first name
-    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     // get last name
-
+    public String getLastName() {
+        return lastName;
+    }
 
     // set last name
-
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     // get full name
-
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 
     // get age
+    public int getAge() {
+        return age;
+    }
 
+    // set age
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     // get occupation
-
+    public String getOccupation() {
+        return occupation;
+    }
 
     // set occupation
-
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
 
     // get department
-
+    public String getDepartment() {
+        return occupation;
+    }
 
     // set department
-
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 
     // get salary
-
+    public double getSalary() {
+        return salary;
+    }
 
     // set salary
-
+    public void setSalary (double salary) {
+        this.salary = salary;
+    }
 
 
 }
