b08899ad261cbb09c83e3964063b03faf844e8a2
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-17 17:39:07 +0800

    Added instance variables under Employee class

---
+++ src/main/java/com/audreynanual/Employee.java
@@ -0,0 +1,47 @@
+// This Employee class contains all the details of an Employee object
+
+package com.audreynanual;
+
+public class Employee {
+    
+    // instance variables
+    private int id;
+    private String firstName;
+    private String secondName;
+    private int age;
+    private String occupation;
+    private String department;
+    private double salary;
+    
+    // getID
+
+
+    // getFirstName
+
+
+    // setFirstName
+    
+
+    // getLastName
+
+
+    // setLastName
+
+
+    // getFullName
+
+
+    // getAge
+
+
+    // getOccupation
+
+
+    // getDepartment
+
+    
+    // getSalary
+
+
+
+}
