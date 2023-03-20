9a7705dceb93560f816bb90793cad5e02ab1ba35
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-20 11:13:04 +0800

    feat: Add a switch statement for user input

--- src/main/java/com/audreynanual/App.java
+++ src/main/java/com/audreynanual/App.java
@@ -6,23 +6,56 @@ public class App {
 
     // Service object
     Service service = new Service();
+
+    // Activated state of the system's "ordering" mode (user will choose a service in the menu)
     static boolean ordering = true;
 
     // Displays menu of system services
     public static void displayMenu() {
         System.out.println("Welcome to the Employee Management System!");
-        System.out.println("\n1. Add Employee");
-        System.out.println("\n2. View Employee");
-        System.out.println("\n3. Update Employee");
-        System.out.println("\n4. Delete Employee");
-        System.out.println("\n5. View All Employees");
-        System.out.println("\n6. Exit");
+        System.out.println("1. Add Employee");
+        System.out.println("2. View Employee");
+        System.out.println("3. Update Employee");
+        System.out.println("4. Delete Employee");
+        System.out.println("5. View All Employees");
+        System.out.println("6. Exit");
     }
 
+    // main method
+    public static void main(String[] args) {
+        // user input
+        Scanner userInput = new Scanner(System.in);
 
-    public static void main( String[] args )
-    {
-        
+        do {
+            displayMenu();
+            
+            System.out.print("\nWhat do you want to do? ");
+            int choice = userInput.nextInt(); // reads value from userInput and stores it in choice
+
+            switch(choice) {
+                case 1:
+                    System.out.println("Add Employee");
+                    break;
+                case 2:
+                    System.out.println("\nView Employee");
+                    break;
+                case 3:
+                    System.out.println("\nUpdate Employee");
+                    break;
+                case 4:
+                    System.out.println("\nDelete Employee");
+                    break;
+                case 5:
+                    System.out.println("\nView All Employees");
+                    break;
+                case 6:
+                    System.out.println("\nThank you for using the application. See you next time!");
+                    System.exit(0);
+                default:
+                    System.out.println("\nPlease enter a valid service from the menu.");
+                    break;
+            }
+        } while (ordering);
     }
 }
 
