680c0799dd58f75d7fce267d5f413bc7c4e08f3e
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-20 13:15:37 +0800

    refactor: Improve loops

--- src/main/java/com/audreynanual/Service.java
+++ src/main/java/com/audreynanual/Service.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Service {
 
     // instance variables
-    String id;
+    int id;
     String firstName;
     String lastName;
     int age;
@@ -14,16 +14,19 @@ public class Service {
     String occupation;
     double salary;
 
+    // user input
+    Scanner userInput = new Scanner(System.in);
+    
     // This HashSet object will contain/store all employee details (since this system does not have a database)
     HashSet<Employee> employeeSet = new HashSet<Employee>();
 
     // employee information
-    Employee employee1 = new Employee("0001", "James", "Smith", 27, "Information Technology", "Developer", 30000);
-    Employee employee2 = new Employee("0002", "Maria", "Rodriguez", 26, "Information Technology", "Tester", 28000);
-    Employee employee3 = new Employee("0003", "David", "Brown", 30, "Administrative", "Receptionist", 25000);
-    Employee employee4 = new Employee("0004", "Elizabeth", "Williams", 29, "Administrative", "Executive Assistant", 35000);
-    Employee employee5 = new Employee("0005", "Thomas", "Clark", 25, "Legal", "Compliance Officer", 37000);
-    Employee employee6 = new Employee("0006", "Hannah", "Miller", 32, "Accounting and Finance", "Accountant", 33000);
+    Employee employee1 = new Employee(13001, "James", "Smith", 27, "Information Technology", "Developer", 30000);
+    Employee employee2 = new Employee(13002, "Maria", "Rodriguez", 26, "Information Technology", "Tester", 28000);
+    Employee employee3 = new Employee(13003, "David", "Brown", 30, "Administrative", "Receptionist", 25000);
+    Employee employee4 = new Employee(13004, "Elizabeth", "Williams", 29, "Administrative", "Executive Assistant", 35000);
+    Employee employee5 = new Employee(13005, "Thomas", "Clark", 25, "Legal", "Compliance Officer", 37000);
+    Employee employee6 = new Employee(13006, "Hannah", "Miller", 32, "Accounting and Finance", "Accountant", 33000);
 
     // constructor method
     public Service() {
@@ -36,11 +39,33 @@ public class Service {
         employeeSet.add(employee6);
     }
 
-    // View all employees
+    // 2. View an employee based on their ID
+    public void viewEmployeeByID() {
+        boolean found = false;
+        System.out.print("\nEnter employee ID: ");
+        id = userInput.nextInt(); 
+        System.out.println("");
+
+        for (Employee employee:employeeSet) {
+            if(employee.getID() == id) {
+                System.out.println(employee);
+                found = true;
+                break;
+            }}
+
+        if (!found) {
+            System.out.println("There is no employee with this ID.\n");
+
+        }
+        // if(employeeSet.size() < userInput) {
+        //     System.out.println("\nThere is no employee with this ID.");
+        // }
+    }
+
+    // 5. View all employees
     public void viewAllEmployees() {
-        // Display all employees and their corresponding information from the employeeSet HashSet 
         for(Employee employee:employeeSet) {
             System.out.println(employee);
-        }
+        }        
     }
 }
