30861a2ec15cb6a0d1080a520f16282d72760af1
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-20 15:11:09 +0800

    feat: Comment out employees 4 to 6

--- src/main/java/com/audreynanual/Service.java
+++ src/main/java/com/audreynanual/Service.java
@@ -27,9 +27,9 @@ public class Service {
     Employee employee1 = new Employee(13001, "James", "Smith", 27, "Information Technology", "Developer", 30000);
     Employee employee2 = new Employee(13002, "Maria", "Rodriguez", 26, "Information Technology", "Tester", 28000);
     Employee employee3 = new Employee(13003, "David", "Brown", 30, "Administrative", "Receptionist", 25000);
-    Employee employee4 = new Employee(13004, "Elizabeth", "Williams", 29, "Administrative", "Executive Assistant", 35000);
-    Employee employee5 = new Employee(13005, "Thomas", "Clark", 25, "Legal", "Compliance Officer", 37000);
-    Employee employee6 = new Employee(13006, "Hannah", "Miller", 32, "Accounting and Finance", "Accountant", 33000);
+    // Employee employee4 = new Employee(13004, "Elizabeth", "Williams", 29, "Administrative", "Executive Assistant", 35000);
+    // Employee employee5 = new Employee(13005, "Thomas", "Clark", 25, "Legal", "Compliance Officer", 37000);
+    // Employee employee6 = new Employee(13006, "Hannah", "Miller", 32, "Accounting and Finance", "Accountant", 33000);
 
     // constructor method
     public Service() {
@@ -37,9 +37,9 @@ public class Service {
         employeeSet.add(employee1);
         employeeSet.add(employee2);
         employeeSet.add(employee3);
-        employeeSet.add(employee4);
-        employeeSet.add(employee5);
-        employeeSet.add(employee6);
+        // employeeSet.add(employee4);
+        // employeeSet.add(employee5);
+        // employeeSet.add(employee6);
     }
 
     // 2: View an employee based on their ID
@@ -145,7 +145,6 @@ public class Service {
 
         for(Employee employee:employeeSet) {
             System.out.println(employee);
-            id = userInput.nextInt();
         }
     }
 }
