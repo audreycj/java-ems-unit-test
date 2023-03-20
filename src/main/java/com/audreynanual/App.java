9aa568f886e289ecda08768f24e34801f8b10dcb
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-20 13:58:20 +0800

    refactor: Remove excess equal signs in displayMenu()

--- src/main/java/com/audreynanual/App.java
+++ src/main/java/com/audreynanual/App.java
@@ -12,9 +12,9 @@ public class App {
 
     // Displays menu of system services
     public static void displayMenu() {
-        System.out.println("==============================");
+        System.out.println("===========================");
         System.out.println("Employee Management System");
-        System.out.println("==============================");
+        System.out.println("===========================");
         System.out.println("1. Add Employee");
         System.out.println("2. View Employee");
         System.out.println("3. Update Employee");
