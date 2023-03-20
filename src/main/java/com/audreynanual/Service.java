ecc35c4fe2b67cf258051ad6ccdb674c11248b56
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-20 11:45:27 +0800

    refactor: Rearrange code blocks

--- src/main/java/com/audreynanual/Service.java
+++ src/main/java/com/audreynanual/Service.java
@@ -5,28 +5,25 @@ import java.util.Scanner;
 
 public class Service {
 
-    // This HashSet object will contain/store all employee details (since this system does not have a database)
-    HashSet<Employee> employeeSet = new HashSet<Employee>();
-
-    // employee information
-    Employee employee1 = new Employee(0001, "James", "Smith", 27, "Information Technology", "Developer", 30000);
-    Employee employee2 = new Employee(0002, "Maria", "Rodriguez", 26, "Information Technology", "Tester", 28000);
-    Employee employee3 = new Employee(0003, "David", "Brown", 30, "Administrative", "Receptionist", 25000);
-    Employee employee4 = new Employee(0004, "Elizabeth", "Williams", 29, "Administrative", "Executive Assistant", 35000);
-    Employee employee5 = new Employee(0005, "Thomas", "Clark", 25, "Legal", "Compliance Officer", 37000);
-    Employee employee6 = new Employee(0006, "Hannah", "Miller", 32, "Accounting and Finance", "Accountant", 33000);
-
-
-    Scanner sc = new Scanner(System.in);
-
     // instance variables
-    int id;
+    String id;
     String firstName;
     String lastName;
     int age;
     String department;
     String occupation;
     double salary;
+
+    // This HashSet object will contain/store all employee details (since this system does not have a database)
+    HashSet<Employee> employeeSet = new HashSet<Employee>();
+
+    // employee information
+    Employee employee1 = new Employee("0001", "James", "Smith", 27, "Information Technology", "Developer", 30000);
+    Employee employee2 = new Employee("0002", "Maria", "Rodriguez", 26, "Information Technology", "Tester", 28000);
+    Employee employee3 = new Employee("0003", "David", "Brown", 30, "Administrative", "Receptionist", 25000);
+    Employee employee4 = new Employee("0004", "Elizabeth", "Williams", 29, "Administrative", "Executive Assistant", 35000);
+    Employee employee5 = new Employee("0005", "Thomas", "Clark", 25, "Legal", "Compliance Officer", 37000);
+    Employee employee6 = new Employee("0006", "Hannah", "Miller", 32, "Accounting and Finance", "Accountant", 33000);
 
     // constructor method
     public Service() {
