1db58b95b1d748b4dabb5631e965b5b7f8392254
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-20 09:55:39 +0800

    feat: Add employee variables under Service class

--- src/main/java/com/audreynanual/Service.java
+++ src/main/java/com/audreynanual/Service.java
@@ -1,7 +1,20 @@
-// contains the logic of each operation the system can do
+// The Service class contains the logic of each operation the system can do
 
 package com.audreynanual;
 
+import java.util.HashSet;
+
 public class Service {
+    // 'HashSet' implements the Set interface and stores unique elements
+    HashSet<Employee> employeeSet = new HashSet<Employee>();
+
+    Employee employee1 = new Employee(0001, "James", "Smith", 27, "Information Technology", "Developer", 30000);
+    Employee employee2 = new Employee(0002, "Maria", "Rodriguez", 26, "Information Technology", "Tester", 28000);
+    Employee employee3 = new Employee(0003, "David", "Brown", 30, "Administrative", "Receptionist", 25000);
+    Employee employee4 = new Employee(0004, "Elizabeth", "Williams", 29, "Administrative", "Executive Assistant", 35000);
+    Employee employee5 = new Employee(0005, "Thomas", "Clark", 25, "Legal", "Compliance Officer", 37000);
+    Employee employee6 = new Employee(0006, "Hannah", "Miller", 32, "Accounting and Finance", "Accountant", 33000);
+
     
+
 }
